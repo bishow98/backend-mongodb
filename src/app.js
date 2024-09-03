2ac7a45ce73d1466809d19da400943d19df4b9fb
Bishow Chauhan <chauhandavid4199@gmail.com>
2024-09-03 18:56:22 +0545

    connected to mongo atlas

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+//yo app.use method le chai middleware mount garna help garxa
+//express.json: build in middleware in express js
+app.use(express.json({ limit: "16kb" }));
+
+/*express.urlencoded is build in middleware it parses the incoming request with urlencoded payloads and is based on body-parser: ex: jastai brower ma search garda + %20 yesto aaunxa teslai handle garxa */
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+//express.static le chai static file lai serve garxa
+app.use(express.static("public"));
+
+//it allows the server to automatically parse cookies sent by the client in the Cookie header of the request.
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // require('dotenv').config()
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||4500, ()=>{
+        console.log(`Server running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log(`Mongo Db connection error : ${error}`);
+})
+
 
 
 
